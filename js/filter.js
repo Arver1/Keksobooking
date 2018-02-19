d006cee2ae998b7c97b90af15b1885b4762200af
Александр <aactepanov@mail.ru>
2018-02-19 14:20:11 +0300

    did filters

--- js/filter.js
+++ js/filter.js
@@ -6,15 +6,10 @@ const houseType = document.querySelector('#housing_type');
 const priceType = document.querySelector('#housing_price');
 const roomNumberType = document.querySelector('#housing_room-number');
 const guestNumberType = document.querySelector('#housing_guests-number');
+const housingFeatures = document.querySelector('#housing_features');
 const map = document.querySelector('.tokyo__pin-map');
 
 
-const filterAds = (data)=> {
-  let tempData = data.filter((item) => {
-    return checkHouseType(item) && checkPriceType(item);// && checkRoomNumberType(item) && checkGuestNumberType(item);
-  });
-  return tempData.length > 5 ? tempData.slice(0, 5) : tempData;
-};
 const checkHouseType = item => {
   const temp = houseType[houseType.selectedIndex].value;
   if (temp === 'any') {
@@ -49,6 +44,43 @@ const checkPriceType = item => {
       break;
   }
   return flag;
+};
+const checkRoomNumberType = item => {
+  const temp = roomNumberType[roomNumberType.selectedIndex].value;
+  if (temp === 'any') {
+    return true;
+  }
+  return item.offer.rooms + '' === roomNumberType[roomNumberType.selectedIndex].value;
+};
+const checkGuestNumberType = item => {
+  const temp = guestNumberType[guestNumberType.selectedIndex].value;
+  if (temp === 'any') {
+    return true;
+  }
+  return item.offer.guests + '' === guestNumberType[guestNumberType.selectedIndex].value;
+};
+const checkHouseFeatures = item => {
+  let flag = true;
+  let itemFeatures = item.offer.features;
+  let checkedFeatures = [...housingFeatures.elements].filter(it => {
+    return it.checked;
+  });
+  if (!checkedFeatures) {
+    return flag;
+  }
+  for (let it of checkedFeatures) {
+    if (!~itemFeatures.indexOf(it.value)) {
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+};
+const filterAds = data => {
+  let tempData = data.filter((item) => {
+    return checkHouseType(item) && checkPriceType(item) && checkRoomNumberType(item) && checkGuestNumberType(item) && checkHouseFeatures(item);
+  });
+  return tempData.length > 5 ? tempData.slice(0, 5) : tempData;
 };
 filters.addEventListener('change', () => {
   for (let it of document.querySelectorAll('.ad')) {
